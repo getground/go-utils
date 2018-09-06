92dac2f732396b97bf7d1a83d983ffe194fe004e
Misrab <faizullah.misrab@gmail.com>
2018-09-06 12:42:12 +0100

    write basic GET, POST requests. no testing

---
+++ request.go
@@ -0,0 +1,44 @@
+package goutils
+
+
+import (
+  "bytes"
+
+  "net/http"
+  "io/ioutil"
+)
+
+
+func GetWithHeaders(url string, headers map[string]string) ([]byte, error) {
+  return makeRequestWithHeaders(url, "GET", nil, headers)
+}
+
+func PostWithHeaders(url string, data []byte, headers map[string]string) ([]byte, error) {
+  return makeRequestWithHeaders(url, "POST", data, headers)
+}
+
+
+func makeRequestWithHeaders(url string, method string, data []byte, headers map[string]string) ([]byte, error) {
+  client := &http.Client{}
+  req, err := http.NewRequest(method, url, bytes.NewBuffer([]byte(data)))
+  if err != nil {
+    return nil, err
+  }
+
+  for k, v := range headers {
+    req.Header.Add(k, v)
+  }
+
+  resp, err := client.Do(req)
+  if err != nil {
+    return nil, err
+  }
+  defer resp.Body.Close()
+
+  resp_data, err := ioutil.ReadAll(resp.Body)
+  if err != nil {
+    return nil, err
+  }
+
+  return resp_data, nil
+}
